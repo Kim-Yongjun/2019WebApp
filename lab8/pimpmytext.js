5109f9e6660f127a099a0ce6e62744ec02f2a28c
김용준 <kim-yongjun@gim-yongjun-ui-MacBook-Pro.local>
2019-11-22 17:12:38 +0900

    unfinished

---
+++ lab8/pimpmytext.js
@@ -0,0 +1,10 @@
+function bigger(){
+    $("inputtext").style.fontSize = "24px";
+}
+
+function check(){
+    $("inputtext").style.color = "green";
+    $("inputtext").style.underline = "underline";
+}
+
+
